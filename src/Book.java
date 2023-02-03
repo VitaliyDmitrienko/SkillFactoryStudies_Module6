821ae79c4d761b87f6120c9efcee6e7f2b9cf9ad
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-02-03 15:34:01 +0300

    5th commit + A14 (solving #2 the problem Book)

--- src/Book.java
+++ src/Book.java
@@ -8,24 +8,18 @@ public class Book extends PrintEdition {
     int year;
     String author;
     String[] pages;
-    String[] pages2;
 
     public Book(String name, int quantityOfPages, int year, String author, String[] pages){
         super(name);
         this.quantityOfPages = quantityOfPages;
         this.year = year;
         this.author = author;
-        pages2 = new String[quantityOfPages];
-        for (int i=pages.length; i > 0; i--) {
-            pages2[i] = pages [i-1];
+        this.pages = new String[quantityOfPages+1];
+        this.pages[0] = name + ", " + author;
+        for (int i=1; i < this.pages.length; i++) {
+            this.pages[i] = pages[i-1];
+            System.out.println(this.pages[i]);
         }
-        this.pages2[0] = name + ", " + author;
-        this.pages = pages2;
-//        this.pages = new String[quantityOfPages];
-//        for (int i=1; i < this.pages.length; i++) {
-//            pages[i] = pages[i-1];
-//            System.out.println(pages[i]);
-//        }
     }
 
     public String getPage(int n) {
